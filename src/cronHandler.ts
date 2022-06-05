6d5f698b0b0b25e1da0bad967da7f9d6c9b72e87
base0010 <marcus@gatekeeper.network>
2022-06-04 21:54:55 -0600

    works with cid 1, needs other chainIds

--- src/cronHandler.ts
+++ src/cronHandler.ts
@@ -26,21 +26,7 @@ export async function getCrosschainHealth(): Promise<Healths | void> {
       const subgraphUrls = chain.subgraph;
       console.log(`ChainID: ${chain.chainId}: SubgraphURL ${subgraphUrls}`);
       //override for mainnet subgraph break
-      if (chain.chainId === 1) {
-        const overrideStatus: SubgraphHealth = {
-          chainHeadBlock: 1,
-          latestBlock: 1,
-          lastHealthyBlock: 1,
-          network: "mainnet",
-          fatalError: undefined,
-          health: "healthy",
-          synced: true,
-          url: `${chain.subgraph[0]}`,
-        };
-        return (healthsByChainId[chain.chainId] = JSON.stringify({
-          data: overrideStatus,
-        }));
-      } else if (subgraphUrls === undefined) {
+       if (subgraphUrls === undefined) {
         console.log(`no configured subgraphs for this chain`);
         return (healthsByChainId[chain.chainId] = JSON.stringify({
           data: undefined,
@@ -49,30 +35,46 @@ export async function getCrosschainHealth(): Promise<Healths | void> {
       if (subgraphUrls) {
         //statues for all urls
         const urlStatuses: SubgraphHealth[] = [];
-
-        await Promise.all(
-          subgraphUrls.map(async (subgraphUrl: string) => {
-            try {
-              console.log(getSubgraphName(subgraphUrl));
-              const status = await getSubgraphHealth(
-                getSubgraphName(subgraphUrl),
-                subgraphUrl
-              );
-              if (status) {
-                status.url = subgraphUrl;
-                urlStatuses.push(status);
-              }
-            } catch (err) {
-              console.error(
-                `Error getting health for subgraph ${getSubgraphName(
+        if (chain.chainId === 1) {
+          const overrideStatus: SubgraphHealth = {
+            chainHeadBlock: 1,
+            latestBlock: 1,
+            lastHealthyBlock: 1,
+            network: "mainnet",
+            fatalError: undefined,
+            health: "healthy",
+            synced: true,
+            url: `${chain.subgraph[0]}`,
+          };
+          urlStatuses.push(overrideStatus);
+          return (healthsByChainId[chain.chainId] = JSON.stringify({
+           urlStatuses,
+          }));
+        } else {
+          await Promise.all(
+            subgraphUrls.map(async (subgraphUrl: string) => {
+              try {
+                console.log(getSubgraphName(subgraphUrl));
+                const status = await getSubgraphHealth(
+                  getSubgraphName(subgraphUrl),
                   subgraphUrl
-                )}: `,
-                err
-              );
-            }
-            healthsByChainId[chain.chainId] = JSON.stringify(urlStatuses);
-          })
-        );
+                );
+                if (status) {
+                  status.url = subgraphUrl;
+                  urlStatuses.push(status);
+                }
+              } catch (err) {
+                console.error(
+                  `Error getting health for subgraph ${getSubgraphName(
+                    subgraphUrl
+                  )}: `,
+                  err
+                );
+              }
+              healthsByChainId[chain.chainId] = JSON.stringify(urlStatuses);
+            })
+          );
+        }
       }
     })
   );
